13d2d273ed12b95e986d7d717f4d4b761080948a
ProjektKris <rbxkrisxyz@gmail.com>
2021-02-05 22:44:58 +0700

    Moved object instance building instructions to build module

--- src/ball.lua
+++ src/ball.lua
@@ -1,57 +1,34 @@
 -- serversideonly
 local std = require(game.ReplicatedStorage.Source.std)
-local function build_ball(client)
-    local ball =
-        std.create("Model", workspace, {Name = tostring(client.UserId)})
-    local sphere = std.create("Part", ball, {
-        Name = "sphere",
-        Size = Vector3.new(5, 5, 5),
-        Position = Vector3.new(0, 0, 0),
-        Shape = Enum.PartType.Ball,
-        CanCollide = true
-    })
-    local center = std.create("Part", ball, {
-        Name = "center",
-        Size = Vector3.new(1, 1, 1),
-        Position = Vector3.new(0, 0, 0),
-        Shape = Enum.PartType.Ball,
-        CanCollide = false
-    })
-    local a0 = std.create("Attachment", center)
-    local a1 = std.create("Attachment", sphere)
-    std.create("BallSocketConstraint", center,
-               {Attachment0 = a0, Attachment1 = a1})
-    std.create("BodyThrust", center, {Force = Vector3.new(0, 0, 0)})
-    std.create("BodyGyro", center)
-
-    ball.PrimaryPart = ball.center
-    ball.PrimaryPart:SetNetworkOwner(client)
-    return ball
-end
+local build = std.import("build")
 local ball = {}
 ball.__index = ball
 function ball.new(client)
-    local new_instance = build_ball(client)
-    local new_object = {instance = new_instance, velocity = 100}
+    local new_instance = build.ball(client)
+    local new_object = {
+        instance = new_instance,
+        max_force = 1000,
+        health = 100
+    }
     setmetatable(new_object, ball)
     return new_object
 end
-function ball:translate_x(positive)
+function ball:apply_force_x(positive)
     if positive then
         self.instance.center.BodyThrust.Force =
-            self.instance.center.BodyThrust.Force + Vector3.new(self.velocity, 0, 0)
+            self.instance.center.BodyThrust.Force + Vector3.new(self.max_force, 0, 0)
     else
         self.instance.center.BodyThrust.Force =
-            self.instance.center.BodyThrust.Force - Vector3.new(self.velocity, 0, 0)
+            self.instance.center.BodyThrust.Force - Vector3.new(self.max_force, 0, 0)
     end
 end
-function ball:translate_y(positive)
+function ball:apply_force_y(positive)
     if positive then
         self.instance.center.BodyThrust.Force =
-            self.instance.center.BodyThrust.Force + Vector3.new(0, 0, self.velocity)
+            self.instance.center.BodyThrust.Force + Vector3.new(0, 0, self.max_force)
     else
         self.instance.center.BodyThrust.Force =
-            self.instance.center.BodyThrust.Force - Vector3.new(0, 0, self.velocity)
+            self.instance.center.BodyThrust.Force - Vector3.new(0, 0, self.max_force)
     end
 end
 function ball:remove()
---
+++ src/build.lua
@@ -0,0 +1,80 @@
+-- replicated
+-- contains all instructions to build instances with script
+-- this is done because this project utilizes rojo
+local std = require(game.ReplicatedStorage.Source.std)
+local build = {}
+function build.ball(client)
+    local instance = std.create("Model", workspace.Players,
+                                {Name = tostring(client.UserId)})
+    local sphere = std.create("Part", instance, {
+        Name = "sphere",
+        Size = Vector3.new(5, 5, 5),
+        Position = Vector3.new(0, 0, 0),
+        Shape = Enum.PartType.Ball,
+        CanCollide = true
+    })
+    local center = std.create("Part", instance, {
+        Name = "center",
+        Size = Vector3.new(1, 1, 1),
+        Position = Vector3.new(0, 0, 0),
+        Shape = Enum.PartType.Ball,
+        CanCollide = false
+    })
+    local a0 = std.create("Attachment", center)
+    local a1 = std.create("Attachment", sphere)
+    std.create("BallSocketConstraint", center,
+               {Attachment0 = a0, Attachment1 = a1})
+    std.create("BodyThrust", center, {Force = Vector3.new(0, 0, 0)})
+    std.create("BodyGyro", center)
+
+    instance.PrimaryPart = instance.center
+    instance.PrimaryPart:SetNetworkOwner(client)
+    return instance
+end
+function build.ui()
+    --[[
+        struct
+        ScreenGui {
+            main_menu {
+                play_buton
+            }
+            ingame {
+                timer_text
+            }
+        }
+    ]]
+    local new_screen_gui = Instance.new("ScreenGui",  game.Players.LocalPlayer.PlayerGui)
+    local main_menu_frame = std.create("Frame", new_screen_gui, {
+        Name = "main_menu",
+        Size = UDim2.new(1,0,1,0),
+        BackgroundTransparency = 1
+    })
+    local ingame_frame = std.create("Frame", new_screen_gui, {
+        Name = "ingame",
+        Size = UDim2.new(1,0,1,0),
+        BackgroundTransparency = 1,
+        Visible = false
+    })
+    local play_button = std.create("TextButton", main_menu_frame, {
+        Name = "play_button",
+        Size = UDim2.new(1,0,1,0),
+        BackgroundTransparency = 1,
+        TextColor3 = Color3.new(1,1,1),
+        TextXAlignment = Enum.TextXAlignment.Center,
+        FontSize = Enum.FontSize.Size42,
+        Font = Enum.Font.RobotoCondensed,
+        Text = "Start!"
+    })
+    local timer_text = std.create("TextLabel", ingame_frame, {
+        Name = "timer_text",
+        Size = UDim2.new(1,0,.1,0),
+        BackgroundTransparency = 1,
+        TextColor3 = Color3.new(1,1,1),
+        TextScaled = true,
+        TextXAlignment = Enum.TextXAlignment.Center,
+        Font = Enum.Font.RobotoCondensed,
+        Text = "0"
+    })
+    return new_screen_gui
+end
+return build
