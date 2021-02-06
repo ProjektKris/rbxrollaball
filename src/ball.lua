d38fecc0ff2a19b48cda37f7f627c7168bb5fbcf
ProjektKris <rbxkrisxyz@gmail.com>
2021-02-06 18:36:41 +0700

    Added on chip collided, remove event

--- src/ball.lua
+++ src/ball.lua
@@ -1,16 +1,30 @@
 -- serversideonly
+local collection_service = game:GetService("CollectionService")
 local std = require(game.ReplicatedStorage.Source.std)
 local build = std.import("build")
 local ball = {}
 ball.__index = ball
 function ball.new(client)
-    local new_instance = build.ball(client)
     local new_object = {
-        instance = new_instance,
+        instance = build.ball(client),
         max_force = 1000,
-        health = 100
+        health = 100,
+        on_remove_func = {},
+        on_points_added_func = {}
     }
     setmetatable(new_object, ball)
+    new_object.instance.sphere.Touched:Connect(function(part)
+        if part then
+            if collection_service:HasTag(part, "chip") then
+                for i = 1, #new_object.on_points_added_func do
+                    local new_thread = coroutine.wrap(function()
+                        new_object.on_points_added_func[i](part)
+                    end)
+                    new_thread()
+                end
+            end
+        end
+    end)
     return new_object
 end
 function ball:apply_force_x(positive)
@@ -32,9 +46,19 @@ function ball:apply_force_y(positive)
     end
 end
 function ball:remove()
-    
+    for i = 1, #self.on_remove_func do
+        local new_thread = coroutine.wrap(function()
+            self.on_remove_func[i]()
+        end)
+        new_thread()
+    end
+    self.instance:Destroy()
+    self = nil
 end
-function ball:on_remove()
-    
+function ball:on_remove(func)
+    self.on_remove_func[#self.on_remove_func+1] = func
+end
+function ball:on_points_added(func)
+    self.on_points_added_func[#self.on_points_added_func+1] = func
 end
 return ball
