70a047c37fac24c50f46b6c1c65abc99ee76aa23
ProjektKris <rbxkrisxyz@gmail.com>
2021-02-06 18:37:30 +0700

    Added gravity settings, chips

--- src/configs.lua
+++ src/configs.lua
@@ -1,4 +1,5 @@
 return {
+    gravity = 35,
     cam_offset = Vector3.new(0, 50, -40),
     modes = {"main_menu", "ingame"}
 }
--- src/server_handler.lua
+++ src/server_handler.lua
@@ -1,17 +1,38 @@
 -- serversideonly
 local std = require(game.ReplicatedStorage.Source.std)
+local configs = std.import("configs")
 local ball = std.import("ball")
+local chip = std.import("chip")
 local module = {}
 function module.init()
+    -- create remotes
     local remotes = std.dir("ReplicatedStorage/remotes")
     local remote_function = Instance.new("RemoteFunction", remotes)
     local remote_event = Instance.new("RemoteEvent", remotes)
+
+    -- set gravity
+    workspace.Gravity = configs.gravity
 
     -- disable player walking
     std.create("Humanoid", game.StarterPlayer, {WalkSpeed = 0})
 
     local balls = {}
+    local chips = {}
 
+    -- load chips
+    for _, node in pairs(workspace.Level.Chip_Nodes:GetChildren()) do
+        if node:IsA("Attachment") then
+            chips[#chips + 1] = chip.new(2, 1, {
+                Anchored = true,
+                CanCollide = false,
+                Position = node.WorldPosition,
+                Size = Vector3.new(4, 4, 4),
+                BrickColor = BrickColor.Yellow()
+            })
+        end
+    end
+
+    -- events
     remote_function.OnServerInvoke = function(client, item, ...)
         if item == "spawn" then
             balls[client.UserId] = ball.new(client)
